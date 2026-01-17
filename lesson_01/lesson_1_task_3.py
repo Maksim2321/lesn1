c0edc66c7c8120f6d6ab5bb9c3ff10d33fc9eb9a
Maksim2321 <trav.max2001@gmai.com>
2026-01-17 22:03:37 +0300

    upd1

--- lesson_01/lesson_1_task_3.py
+++ lesson_01/lesson_1_task_3.py
@@ -1,3 +1,3 @@
-first_name = input("Максим")
-last_name = input("Травников")
+first_name = input("Введите имя: ")
+last_name = input("Введите фамилию: ")
 print(f"Вас зовут: {last_name} {first_name}")
